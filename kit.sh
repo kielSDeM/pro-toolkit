3498b8b02855adc7f6adc6e34a2afa98649c2615
kiel <ksdcastor1219@gmail.com>
2022-01-18 23:38:20 -0700

    made some changes to the kit.sh file and editing out some of the files as well

--- kit.sh
+++ kit.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 ./pro-lan.sh 
 ./alfa.sh 
-./meta3.sh 
 ./metasploit.sh
-./mini-conda.sh
+./meta3.sh 
 python3 dev_ops_installer.py
+echo "terraform version: "
+terraform -v
+echo "metasploit version:"
+msfconsole --version
+echo "npm version: "
+npm -v
+echo "node version: "
+node --version
+echo "Python version: "
+python3
+echo "Rust version: "
+rustc --version
+echo "Nmap version:"
+nmap --version
+echo "Wireshark Version:"
+wireshark -v
--- metasploit.sh
+++ metasploit.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+echo "This script will install some pen testing tools for me to learn"
 sudo apt install nmap
+sudo apt install aircrack-ng
+sudo apt install wireshark
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
   chmod 755 msfinstall && \
   ./msfinstall
