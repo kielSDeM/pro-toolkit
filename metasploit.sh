211cdb2cfe06360ab613642618b8c52398525110
KielSD <73503331+kielSDeM@users.noreply.github.com>
2022-01-23 11:54:10 -0700

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo "This script will install some pen testing tools for me to learn"
-sudo apt install nmap
-sudo apt install aircrack-ng
-sudo apt install wireshark
-curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
-  chmod 755 msfinstall && \
-  ./msfinstall
+  declare -a recon=(`sudo apt install nmap` `sudo apt install aircrack-ng` 
+  `sudo apt install wireshark` `curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
+  chmod 755 msfinstall && \` `./msfinstall`)
+  
+  for i in recon:
+      do
+      recon[i++]
+      done
