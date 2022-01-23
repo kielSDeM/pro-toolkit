96d5d6b6661f815d34664feda98a6c9965cabdab
KielSD <73503331+kielSDeM@users.noreply.github.com>
2022-01-23 12:00:32 -0700

    Update alfa.sh

--- alfa.sh
+++ alfa.sh
@@ -6,3 +6,7 @@ git clone https://github.com/aircrack-ng/rtl8812au.git
 cd rtl8812au
 make
 sudo make install
+
+declare -a alfa(`sudo apt install build-essentials` `sudo apt install bc` 
+`sudo apt install linux-headers-`uname -r`` `git clone https://github.com/aircrack-ng/rtl8812au.git`
+`cd rtl8812au ; make ; sudo make install`)
