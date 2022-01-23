97592f8bee36efd7d022be590142bb6822f02751
KielSD <73503331+kielSDeM@users.noreply.github.com>
2022-01-23 12:05:11 -0700

    Update alfa.sh

--- alfa.sh
+++ alfa.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-sudo apt install build-essential
-sudo apt install bc
-sudo apt install linux-headers-`uname -r`
-git clone https://github.com/aircrack-ng/rtl8812au.git
-cd rtl8812au
-make
-sudo make install
-
 declare -a alfa(`sudo apt install build-essentials` `sudo apt install bc` 
 `sudo apt install linux-headers-`uname -r`` `git clone https://github.com/aircrack-ng/rtl8812au.git`
 `cd rtl8812au ; make ; sudo make install`)
+
+for i in "${#alfa[@]}";
+    do
+    alfa[i++]
+    done
