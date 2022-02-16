37e25e297100cd226a1e5a372a70cd78ed9521c5
Kiel <ksdcastor1219@hotmail.com>
2022-02-16 01:46:14 -0700

    believe this is the best version to date

--- kit.sh
+++ kit.sh
@@ -3,11 +3,11 @@ echo "going to install my favorite tools for programming"
 sudo apt-get update ; sudo apt-get upgrade -y ; sudo apt-get dist-upgrade -y
 #This installs my programming languages, ides and virtual enviroments
 cd py_scripts ; python3 py_run.py
-declare -A prolang='(["metasploit version: "]=`msfconsole --version`
-["nmap version: "]=`nmap -v` ["wireshark version: "]=`wireshark --version`
+declare -A prolang='(["nmap version: "]=`nmap -v` 
+["wireshark version: "]=`wireshark --version`
 ["python verison: "]=`python3 --version`
-["Rust version: "]=`rustc --version` ["node version: "]= `node -v`
-["npm version: "]= `npm -v`)'
+["Rust version: "]=`rustc --version` ["node version: "]=`node --version` 
+["npm version: "]=`npm --version`)'
 for i in "${!prolang[@]}"; 
     do echo "$i ${prolang[$i]}"; 
     done
