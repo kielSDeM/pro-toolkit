f7d70d6139afe72f9c4b73f8daecaa15e6f49c03
KielSD <73503331+kielSDeM@users.noreply.github.com>
2022-02-25 16:33:10 -0700

    Update qemu.py

--- text_files/qemu.py
+++ text_files/qemu.py
@@ -3,6 +3,6 @@
                   'git submodule init; git submodule update --recursive','sudo apt-get install ninja-build',
                   'sudo apt-get install -y libglib2.0-dev; sudo apt-get install -y libpixman-1-dev',
                   'sudo apt-get install build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev',
-                  'sudo apt-get install libavahi-ui-gtk3-dev'
+                  'sudo apt-get install libavahi-ui-gtk3-dev', 'sudo apt install gtk-3-examples'
                  ]
         return command
